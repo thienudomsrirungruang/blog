8cf2796faf1cc3c70c6083d702602926610676d3
Thien <thien.udomsrirungruang@gmail.com>
2017-08-04 15:46:51 +0700

    Added js for post page

--- src/main/resources/static/js/post.js
+++ src/main/resources/static/js/post.js
@@ -17,5 +17,31 @@ function shakePicture(){
 
 
 function requestContent(){
-    
+    let response;
+    $.ajax({
+        method: 'POST',
+        url: '/post-info',
+        data: {id: 0}
+    }).done(function(data){
+        response = data;
+        setContent(response);
+    })
+}
+
+function setContent(content){
+    let date = $('#date');
+    let title = $('#title');
+    let post = $('#post');
+    date.html(function(){
+        let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let postDate = new Date(content.lastMntDate);
+        let dateString = postDate.getDate() + ' ' + monthNames[postDate.getMonth()] + ' ' + (postDate.getYear() + 1900);
+        return dateString;
+    })
+    title.html(function(){
+        return content.title;
+    })
+    post.html(function(){
+        return content.content;
+    })
 }
